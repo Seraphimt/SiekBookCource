b8a13f50137a8fac0a4661dab3a50d5327d7320b
Seraphimt <svet58585@mail.ru>
2026-06-22 19:43:39 +0300

    Add new regalloc tests

--- registers-alloc.rkt
+++ registers-alloc.rkt
@@ -89,13 +89,64 @@
   (Instr 'movq (list (Imm 24) (Var 'a)))
   (Instr 'movq (list (Imm 18) (Var 'b)))
   (Instr 'addq (list (Var 'a) (Var 'b)))
-  (Instr' movq (list (Var 'b) (Reg 'rax)))
+  (Instr 'movq (list (Var 'b) (Reg 'rax)))
   (Jmp 'conclusion)))
 
 (define etalon-liveness-1 
 (list
   (set (Var 'a))
   (set (Var 'a) (Var 'b))
+  (set (Var 'b))
+  (set) ;from jmp
+  (set) ;from start base
+))
+;-------------------------------------------------------------------------
+(define test-liveness-2 
+(list     
+  (Instr 'movq (list (Imm 52) (Var 'a)))
+  (Instr 'movq (list (Imm 10) (Var 'b)))
+  (Instr 'subq (list (Var 'a) (Var 'b)))
+  (Instr 'movq (list (Var 'b) (Reg 'rax)))
+  (Jmp 'conclusion)))
+
+(define etalon-liveness-2 
+(list
+  (set (Var 'a))
+  (set (Var 'a) (Var 'b))
+  (set (Var 'b))
+  (set) ;from jmp
+  (set) ;from start base
+))
+;-------------------------------------------------------------------------
+(define test-liveness-3 
+(list     
+  (Instr 'movq (list (Imm 99) (Var 'a)))
+  (Instr 'movq (list (Imm 10) (Var 'b)))
+  (Instr 'addq (list (Imm 32) (Var 'b)))
+  (Instr 'movq (list (Var 'b) (Reg 'rax)))
+  (Jmp 'conclusion)))
+
+(define etalon-liveness-3 
+(list
+  (set)
+  (set (Var 'b))
+  (set (Var 'b))
+  (set) ;from jmp
+  (set) ;from start base
+))
+;-------------------------------------------------------------------------
+(define test-liveness-4 
+(list     
+  (Instr 'movq (list (Imm 42) (Var 'a)))
+  (Instr 'movq (list (Imm 10) (Var 'b)))
+  (Instr 'movq (list (Var 'a) (Var 'b)))
+  (Instr 'movq (list (Var 'b) (Reg 'rax)))
+  (Jmp 'conclusion)))
+
+(define etalon-liveness-4 
+(list
+  (set)
+  (set (Var 'b))
   (set (Var 'b))
   (set) ;from jmp
   (set) ;from start base
@@ -113,18 +164,24 @@
   (list 
     (create-test-case test-liveness-0)
     (create-test-case test-liveness-1)
+    (create-test-case test-liveness-2)
+    (create-test-case test-liveness-3)
   ))
   
 (define etalon-suit-liveness
   (list 
     etalon-liveness-0
     etalon-liveness-1
+    etalon-liveness-2
+    etalon-liveness-3
   ))
 
 (define test-No ;temp
   (list 
+    0
     1
     2
+    3
   ))
 ;-------------------------------------------------------------------------
 (define (check-live test-suit etalon-suit)
